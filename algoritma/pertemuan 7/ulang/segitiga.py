c27c3adf77728204bda59508e6fe73075e8b43a2
rratuan <ratua4820@gmail.com>
2023-12-04 19:21:20 +0700

    menambah komentar

--- algoritma/pertemuan 7/ulang/segitiga.py
+++ algoritma/pertemuan 7/ulang/segitiga.py
@@ -1,5 +1,15 @@
 n = int(input('N : '))
 
+# pyramid
+# preview
+#     *     => i(1) = 1 ---- spasi = 4
+#    ***    => i(2) = 3 ---- spasi = 3
+#   *****   => i(3) = 5 ---- spasi = 2
+#  *******  => i(4) = 7 ---- spasi = 1
+# ********* => i(5) = 9 ---- spasi = 0
+# spasi = n-i
+# rumus = 2*i-1
+
 for i in range(1,n+1):
     segitiga = ' '*(n-i) + '*'*(2*i-1)
     print(segitiga)
