02018b1e1049a6205ffb883a5f04f81b1433403e
rratuan <ratua4820@gmail.com>
2023-12-04 17:33:08 +0700

    menambah game tebak angka

--- algoritma/pertemuan 7/ulang/tebakAngka.py
+++ algoritma/pertemuan 7/ulang/tebakAngka.py
@@ -1,8 +1,15 @@
 import random
 
 komputer = random.randint(1,10)
+print(komputer)
 gameStart = True
 
 while gameStart:
-    print('hallo')
-    gameStart = False
+    tebak = int(input('Tebakan anda [1..10] ? '))
+    if tebak > komputer:
+        print('Salah.Tebakan anda Terlalu Besar')
+    elif tebak < komputer:
+        print('Salah.Tebakan anda Terlalu Kecil')
+    else:
+        print('Selamat.Tebakan Anda Benar')
+        gameStart = False
